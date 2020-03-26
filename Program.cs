9e5474c1cfcd546766755e1905afbcf545827bd7
ziutyr <andreyvr98@yandex.ru>
2020-03-26 18:53:09 +0300

    Создана предварительная версия программы

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace TextTonality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = Console.ReadLine();
+            string fileText = File.ReadAllText(file, Encoding.GetEncoding(1251));
+
+            //формирование словаря тональности
+            //определение тональности
+        }
+    }
+}
